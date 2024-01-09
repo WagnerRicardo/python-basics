30c9fca3e8e8055adae9470bbff04193219aee68
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2024-01-09 11:14:20 -0300

    Python básico
    Exercício sobre quebra de laços em Python do Curso em vídeo.

--- ex070.py
+++ ex070.py
@@ -5,7 +5,9 @@ print('-'*32)
 totGasto = 0
 prodMil = 0
 prodBarato = ''
-prodAnterior = 0
+prodBaratoVal = 0
+lista = []
+quantidade = 0
 
 while True:
     #CADASTRO DE PRODUTO
@@ -18,8 +20,23 @@ while True:
         except:
             print('Digite um valor númerico.')
             pass
+    #lista
+    quantidade += 1
+    lista.append([valor, produto])
     #TRATAMENTO
     totGasto += valor
+    if valor > 1000:
+        prodMil += 1
+
+    for i in range(0, quantidade):
+        if i > 0:
+            if lista[i][0] < lista[i - 1][0]:
+                prodBarato = lista[i][1]
+                prodBaratoVal = lista[i][0]
+        else:
+            prodBarato = lista[i][1]
+            prodBaratoVal = lista[i][0]
+
     #CONTINUAR PROGRAMA?
     while True:
         continuar = input('Quer continuar? (S/N)').strip().upper()
@@ -27,3 +44,8 @@ while True:
             break
     if continuar == 'N':
         break
+
+print('-'*10, 'FIM DO PROGRAMA','-'*10)
+print(f'O Total da compra foi R${totGasto:.2f}')
+print(f'Temos {prodMil} Produtos acima de R$1000,00')
+print(f'O produto mais barato foi {prodBarato} no valor de R${prodBaratoVal:.2f}')
